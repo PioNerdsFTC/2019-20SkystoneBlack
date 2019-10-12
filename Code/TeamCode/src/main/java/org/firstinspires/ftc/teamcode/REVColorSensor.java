c6b46f08610c96486400d9a654ba3bf1b2c600a1
Jason_Hung <51948967+JasonHung0527@users.noreply.github.com>
2019-10-12 14:37:00 -0500

    Adjusted REVColorSensor Comments
    Removed unneeded parts

--- Code/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/REVColorSensor.java
+++ Code/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/REVColorSensor.java
@@ -11,18 +11,6 @@ import com.qualcomm.robotcore.hardware.DistanceSensor;
 import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 import java.util.Locale;
 
-/*
- * This is an example LinearOpMode that shows how to use
- * the REV Robotics Color-Distance Sensor.
- *
- * It assumes the sensor is configured with the name "sensor_color_distance".
- *
- * Use Android Studio to Copy this Class, and Paste it into your team's code folder with a new name.
- * Remove or comment out the @Disabled line to add this opmode to the Driver Station OpMode list.
- */
-
-//@TeleOp
-// @Disabled                            // Comment this out to add to the opmode list
 public class REVColorSensor {
     ColorSensor sensorColor;
     // read RGB values from sensorColorIn
