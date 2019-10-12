390b2ba66023df630e3b3daf64cdc1a295aca34a
Jason_Hung <51948967+JasonHung0527@users.noreply.github.com>
2019-10-12 14:04:57 -0500

    Redo JTeleOp & MotorEncoder
    Redo JTeleOp & MotorEncoder

--- Code/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/REVColorSensor.java
+++ Code/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/REVColorSensor.java
@@ -1,18 +1,14 @@
 package org.firstinspires.ftc.teamcode;
-//import com.qualcomm.robotcore.hardware.ColorSensor;
-
+import com.qualcomm.robotcore.hardware.ColorSensor;
 import android.app.Activity;
 import android.graphics.Color;
 import android.view.View;
-
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.ColorSensor;
 import com.qualcomm.robotcore.hardware.DistanceSensor;
-
 import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
-
 import java.util.Locale;
 
 /*
