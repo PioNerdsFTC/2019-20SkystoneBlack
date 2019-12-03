104a22bd61684cb5585d138b0e688eeb29733612
Jason_Hung <51948967+JasonHung0527@users.noreply.github.com>
2019-12-03 16:16:45 -0600

    Added Gyro
    - Created and used Gyro in attempt to turn robot during autonomous period
    
    Gyro documentation: https://modernroboticsinc.com/download/steering-with-the-modern-robotics-gyro/

--- Code/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autobot.java
+++ Code/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autobot.java
@@ -1,5 +1,6 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.hardware.modernrobotics.ModernRoboticsI2cGyro;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.HardwareMap;
@@ -9,11 +10,14 @@ import com.qualcomm.robotcore.util.ElapsedTime;
 @Autonomous
 public class Autobot {
     /* Public OpMode members. */
-    public DcMotor  leftDrive   = null;
-    public DcMotor  rightDrive  = null;
+    public DcMotor  FLeft   = null;
+    public DcMotor  FRight  = null;
+    public DcMotor  BLeft    = null;
+    public DcMotor  BRight   = null;
     public DcMotor  leftArm     = null;
     public Servo    leftClaw    = null;
     public Servo    rightClaw   = null;
+
 
     public static final double MID_SERVO       =  0.5 ;
     public static final double ARM_UP_POWER    =  0.45 ;
@@ -34,21 +38,29 @@ public class Autobot {
         hwMap = ahwMap;
 
         // Define and Initialize Motors
-        leftDrive  = hwMap.get(DcMotor.class, "left_drive");
-        rightDrive = hwMap.get(DcMotor.class, "right_drive");
+        FLeft  = hwMap.get(DcMotor.class, "front_left");
+        FRight = hwMap.get(DcMotor.class, "front_right");
+        BLeft = hwMap.get(DcMotor.class, "back_left");
+        BRight = hwMap.get(DcMotor.class, "back_right");
         leftArm    = hwMap.get(DcMotor.class, "left_arm");
-        leftDrive.setDirection(DcMotor.Direction.FORWARD); // Set to REVERSE if using AndyMark motors
-        rightDrive.setDirection(DcMotor.Direction.REVERSE);// Set to FORWARD if using AndyMark motors
+        FLeft.setDirection(DcMotor.Direction.FORWARD); // Set to REVERSE if using AndyMark motors
+        FRight.setDirection(DcMotor.Direction.REVERSE);// Set to FORWARD if using AndyMark motors
+        BLeft.setDirection(DcMotor.Direction.REVERSE);// Set to FORWARD if using AndyMark motors
+        BRight.setDirection(DcMotor.Direction.REVERSE);// Set to FORWARD if using AndyMark motors
 
         // Set all motors to zero power
-        leftDrive.setPower(0);
-        rightDrive.setPower(0);
+        FLeft.setPower(0);
+        FRight.setPower(0);
+        BLeft.setPower(0);
+        BRight.setPower(0);
         leftArm.setPower(0);
 
         // Set all motors to run without encoders.
         // May want to use RUN_USING_ENCODERS if encoders are installed.
-        leftDrive.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-        rightDrive.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        FLeft.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        FRight.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        BLeft.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        BRight.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         leftArm.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
 
         // Define and initialize ALL installed servos.
--- Code/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/JasonAutonomous.java
+++ Code/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/JasonAutonomous.java
@@ -1,46 +1,58 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.hardware.modernrobotics.ModernRoboticsI2cGyro;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
-public class JasonAutonomous {
+@Autonomous(name="JasonAuto", group="Pushbot")
+public class JasonAutonomous extends LinearOpMode{
     Autobot robot = new Autobot();
     private ElapsedTime runtime = new ElapsedTime();
+    ModernRoboticsI2cGyro gyro = null;
+    static final double FORWARD_SPEED = 0.6;
+    static final double TURN_SPEED    = 0.5;
 
-    static final double     FORWARD_SPEED = 0.6;
-    static final double     TURN_SPEED    = 0.5;
+    public void runOpMode() {
+        gyro = (ModernRoboticsI2cGyro)hardwareMap.gyroSensor.get("gyro");
+        double initialDriveForwardTime = 1.5;
+        double secondDriveForwardTime = 3.0;
+        double initialGyroValue = gyro.getHeading();
 
+        // 1. Drive straight for X seconds
+        robot.FLeft.setPower(FORWARD_SPEED);
+        robot.FRight.setPower(FORWARD_SPEED);
+        robot.BLeft.setPower(FORWARD_SPEED);
+        robot.BRight.setPower(FORWARD_SPEED);
+        runtime.reset();
+        while (opModeIsActive() && (runtime.seconds() < initialDriveForwardTime)) {
+            telemetry.addData("Path", "Leg 1: %2.5f S Elapsed", runtime.seconds());
+            telemetry.update();
+        }
 
+        // 2. Pick up block
 
-    // 1. Drive straight for X seconds
-    robot.leftDrive.setPower(FORWARD_SPEED);
-    robot.rightDrive.setPower(FORWARD_SPEED);
-    runtime.reset();
-    while (opModeIsActive() && (runtime.seconds() < 3.0)) {
-        telemetry.addData("Path", "Leg 1: %2.5f S Elapsed", runtime.seconds());
-        telemetry.update();
+        // 3. Turn Left 90 degrees
+        robot.FLeft.setPower(-TURN_SPEED);
+        robot.FRight.setPower(+TURN_SPEED);
+        robot.BLeft.setPower(-TURN_SPEED);
+        robot.BRight.setPower(+TURN_SPEED);
+        runtime.reset();
+        while (opModeIsActive() && (gyro.getHeading() < initialGyroValue + 90)) {
+            telemetry.addData("Path", "Leg 2: %2.5f S Elapsed", runtime.seconds());
+            telemetry.update();
+        }
+        // 4. Move forward X seconds
+        robot.FLeft.setPower(FORWARD_SPEED);
+        robot.FRight.setPower(FORWARD_SPEED);
+        robot.BLeft.setPower(FORWARD_SPEED);
+        robot.BRight.setPower(FORWARD_SPEED);
+        runtime.reset();
+        while (opModeIsActive() && (runtime.seconds() < secondDriveForwardTime)) {
+            telemetry.addData("Path", "Leg 1: %2.5f S Elapsed", runtime.seconds());
+            telemetry.update();
+        }
+        // 5. Put down block
     }
-
-    // 2. Pick up block
-
-    // 3. Turn Left 90 degrees
-    robot.leftDrive.setPower(-TURN_SPEED);
-    robot.rightDrive.setPower(+TURN_SPEED);
-    runtime.reset();
-    while (opModeIsActive() && (runtime.seconds() < 1.3)) {
-        telemetry.addData("Path", "Leg 2: %2.5f S Elapsed", runtime.seconds());
-        telemetry.update();
-    }
-    // 4. Move forward X seconds
-    robot.leftDrive.setPower(FORWARD_SPEED);
-    robot.rightDrive.setPower(FORWARD_SPEED);
-    runtime.reset();
-    while (opModeIsActive() && (runtime.seconds() < 3.0)) {
-        telemetry.addData("Path", "Leg 1: %2.5f S Elapsed", runtime.seconds());
-        telemetry.update();
-    }
-    // 5. Put down block
-
 }
