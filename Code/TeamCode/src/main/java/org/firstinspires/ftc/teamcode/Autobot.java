495a9a0229396d380102744dfe6e764ee339e524
Jason_Hung <51948967+JasonHung0527@users.noreply.github.com>
2019-12-12 16:46:35 -0600

    Encoder changes
    take a look at telemetry.addData

--- Code/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autobot.java
+++ Code/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autobot.java
@@ -9,47 +9,109 @@ import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.ElapsedTime;
 import com.qualcomm.robotcore.hardware.ColorSensor;
+import org.firstinspires.ftc.robotcore.external.Telemetry;
 
 // Source: HardwarePushbot
 
 public class Autobot {
     /* Public OpMode members. */
-    public DcMotor  FLeft   = null;
-    public DcMotor  FRight  = null;
-    public DcMotor  BLeft    = null;
-    public DcMotor  BRight   = null;
-    public DcMotor  leftArm     = null;
-    public Servo    leftClaw    = null;
-    public Servo    rightClaw   = null;
-    public BNO055IMU imu = null; // ?
-    public ColorSensor colorSensor;
-
     public static final double MID_SERVO       =  0.5 ;
     public static final double ARM_UP_POWER    =  0.45 ;
     public static final double ARM_DOWN_POWER  = -0.45 ;
+
+    public DcMotor  FLeft;
+    public DcMotor  FRight;
+    public DcMotor  BLeft;
+    public DcMotor  BRight;
+    public DcMotor  leftArm;
+    public Servo leftClaw;
+    public Servo    rightClaw;
+    public BNO055IMU imu;
+    public ColorSensor colorSensor;
+    static final double FORWARD_SPEED = 0.6;
+    static final double TURN_SPEED = 0.4;
+    int rotation, startPosition;
 
     /* local OpMode members. */
     HardwareMap hwMap           =  null;
     private ElapsedTime period  = new ElapsedTime();
 
     /* Constructor */
-    public Autobot() {
+    public Autobot(DcMotor FLeftIn, DcMotor FRightIn, DcMotor BLeftIn, DcMotor BRightIn, ColorSensor colorSensorIn, BNO055IMU imuIn) {
+        FLeft = FLeftIn;
+        FRight = FRightIn;
+        BLeft = BLeftIn;
+        BRight = BRightIn;
+        colorSensor = colorSensorIn;
+        imu = imuIn;
 
+//        leftArm = hwMap.get(DcMotor.class, "left_arm");
+        FLeft.setDirection(DcMotor.Direction.FORWARD);
+        FRight.setDirection(DcMotor.Direction.REVERSE);
+        BLeft.setDirection(DcMotor.Direction.FORWARD);
+        BRight.setDirection(DcMotor.Direction.REVERSE);
+
+        // Set all motors to zero power
+        FLeft.setPower(0);
+        FRight.setPower(0);
+        BLeft.setPower(0);
+        BRight.setPower(0);
+//        leftArm.setPower(0);
+    }
+
+    public void driveToBlue() {
+        FLeft.setPower(FORWARD_SPEED);
+        FRight.setPower(FORWARD_SPEED);
+        BLeft.setPower(FORWARD_SPEED);
+        BRight.setPower(FORWARD_SPEED);
+        if (colorSensor.blue() > 500) {
+            FLeft.setPower(0);
+            FRight.setPower(0);
+            BLeft.setPower(0);
+            BRight.setPower(0);
+        }
+    }
+
+    public void driveToRed() {
+        FLeft.setPower(FORWARD_SPEED);
+        FRight.setPower(FORWARD_SPEED);
+        BLeft.setPower(FORWARD_SPEED);
+        BRight.setPower(FORWARD_SPEED);
+        if (colorSensor.red() > 500) {
+            FLeft.setPower(0);
+            FRight.setPower(0);
+            BLeft.setPower(0);
+            BRight.setPower(0);
+        }
+    }
+
+    public void grab() {
+        // Grabber grab method
+    }
+
+
+
+    public void driveByRotation(int rotation) {
+        // Drive by rotations (inches)
+        startPosition = BLeft.getCurrentPosition();
+
+        while (startPosition < rotation) {
+            startPosition = BLeft.getCurrentPosition();
+            FLeft.setPower(FORWARD_SPEED);
+            FRight.setPower(FORWARD_SPEED);
+            BLeft.setPower(FORWARD_SPEED);
+            BRight.setPower(FORWARD_SPEED);
+        }
+        FLeft.setPower(0);
+        FRight.setPower(0);
+        BLeft.setPower(0);
+        BRight.setPower(0);
     }
 
     /* Initialize standard Hardware interfaces */
-    public void init(HardwareMap ahwMap) {
+/*    public void init(HardwareMap ahwMap) {
         // Save reference to Hardware map
         hwMap = ahwMap;
-
-        // Define and Initialize Motors + color sensor
-        FLeft  = hwMap.get(DcMotor.class, "FLeft");
-        FRight = hwMap.get(DcMotor.class, "FRight");
-        BLeft = hwMap.get(DcMotor.class, "BLeft");
-        BRight = hwMap.get(DcMotor.class, "BRight");
-        colorSensor = hwMap.get(ColorSensor.class, "colorSensor");
-        imu = hwMap.get(BNO055IMU.class, "imu");
-
 
 //        leftArm    = hwMap.get(DcMotor.class, "left_arm");
         FLeft.setDirection(DcMotor.Direction.FORWARD);
@@ -66,10 +128,7 @@ public class Autobot {
 
         // Set all motors to run without encoders.
         // May want to use RUN_USING_ENCODERS if encoders are installed.
-        FLeft.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-        FRight.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-        BLeft.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-        BRight.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
 //        leftArm.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
 
         // Define and initialize ALL installed servos.
@@ -79,5 +138,6 @@ public class Autobot {
 //        rightClaw.setPosition(MID_SERVO);
 
 
-    }
+    } */
+
 }
--- Code/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/JasonAutonomous.java
+++ Code/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/JasonAutonomous.java
@@ -5,71 +5,87 @@ import com.qualcomm.hardware.modernrobotics.ModernRoboticsI2cGyro;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.ColorSensor;
+import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
 @Autonomous(name="JasonAuto", group="Pushbot")
 public class JasonAutonomous extends LinearOpMode {
-    HardwareMap hwMap;
-    Autobot robot = new Autobot();
-    private ElapsedTime runtime = new ElapsedTime();
-    BNO055IMU imu;
 
+    private ElapsedTime runtime = new ElapsedTime();
+    public DcMotor  FLeft;
+    public DcMotor  FRight;
+    public DcMotor  BLeft;
+    public DcMotor  BRight;
+    public DcMotor  leftArm;
+    public Servo leftClaw;
+    public Servo    rightClaw;
+    public BNO055IMU imu;
+    public ColorSensor colorSensor;
     static final double FORWARD_SPEED = 0.6;
-    static final double TURN_SPEED = 0.5;
+    static final double TURN_SPEED = 0.4;
+
 
     public void runOpMode() {
-        robot.init(hwMap);
-        imu = hwMap.get(BNO055IMU.class, "imu");
+        // Define and Initialize Motors + color sensor
+        FLeft  = hardwareMap.get(DcMotor .class, "FLeft");
+        FRight = hardwareMap.get(DcMotor.class, "FRight");
+        BLeft = hardwareMap.get(DcMotor.class, "BLeft");
+        BRight = hardwareMap.get(DcMotor.class, "BRight");
+        colorSensor = hardwareMap.get(ColorSensor .class, "colorSensor");
+        imu = hardwareMap.get(BNO055IMU.class, "imu");
+
+        Autobot robot = new Autobot(FLeft, FRight, BLeft, BRight, colorSensor, imu);
+//        imu = hardwareMap.get(BNO055IMU.class, "imu");
         Gyro gyro = new Gyro(imu);
 
-        double initialDriveForwardTime = 3;
+        double initialDriveForwardTime = 2;
         //       double secondDriveForwardTime = 3.0;
         double initialGyroValue = gyro.getZ();
 
         // 1. Drive straight for X seconds
-        robot.FLeft.setPower(FORWARD_SPEED);
-        robot.FRight.setPower(FORWARD_SPEED);
-        robot.BLeft.setPower(FORWARD_SPEED);
-        robot.BRight.setPower(FORWARD_SPEED);
+//        robot.FLeft.setPower(FORWARD_SPEED);
+//        robot.FRight.setPower(FORWARD_SPEED);
+//        robot.BLeft.setPower(FORWARD_SPEED);
+//        robot.BRight.setPower(FORWARD_SPEED);
+//        runtime.reset();
+//        while ((runtime.seconds() < initialDriveForwardTime)) {
+//            telemetry.addData("Path", "Leg 1: %2.5f S Elapsed", runtime.seconds());
+//            telemetry.update();
+//        }
 
-        runtime.reset();
-        while ((runtime.seconds() < initialDriveForwardTime)) {
-            telemetry.addData("Path", "Leg 1: %2.5f S Elapsed", runtime.seconds());
+        // Drive by X rotation
+        while (runtime.seconds()<10000) {
+            telemetry.addData("encoder", robot.BLeft.getCurrentPosition());
             telemetry.update();
         }
 
-        // Stop Robot if blue
-/*        if (robot.colorSensor.blue() == 0) {
-            robot.FLeft.setPower(0);
-            robot.FRight.setPower(0);
-            robot.BLeft.setPower(0);
-            robot.BRight.setPower(0);
-        }
-*/
-        robot.FLeft.setPower(0);
-        robot.FRight.setPower(0);
-        robot.BLeft.setPower(0);
-        robot.BRight.setPower(0);
-        // 2. Lower Grabber, Pick up block, Lift Grabber
+//        robot.driveByRotation(2);
 
-        // 3. Back up
-
-        // 4. Turn Left/Right 90 degrees
-        robot.FLeft.setPower(-TURN_SPEED);
-        robot.FRight.setPower(+TURN_SPEED);
-        robot.BLeft.setPower(-TURN_SPEED);
-        robot.BRight.setPower(+TURN_SPEED);
-        runtime.reset();
-        while ((gyro.getZ() < initialGyroValue + 90)) {
-            telemetry.addData("Path", "Leg 2: %2.5f S Elapsed", runtime.seconds());
-            telemetry.update();
-        }
-        robot.FLeft.setPower(0);
-        robot.FRight.setPower(0);
-        robot.BLeft.setPower(0);
-        robot.BRight.setPower(0);
-        runtime.reset();
+//        // Stop Robot if blue
+//        robot.driveToBlue();
+//        runtime.reset();
+//        // 2. Lower Grabber, Pick up block, Lift Grabber
+//
+//        // 3. Back up
+//
+//        // 4. Turn Left/Right 90 degrees
+//        robot.FLeft.setPower(-TURN_SPEED);
+//        robot.FRight.setPower(+TURN_SPEED);
+//        robot.BLeft.setPower(-TURN_SPEED);
+//        robot.BRight.setPower(+TURN_SPEED);
+//        runtime.reset();
+//        while ((gyro.getZ() < initialGyroValue + 90)) {
+//            telemetry.addData("Path", "Leg 2: %2.5f S Elapsed", runtime.seconds());
+//            telemetry.update();
+//        }
+//        robot.FLeft.setPower(0);
+//        robot.FRight.setPower(0);
+//        robot.BLeft.setPower(0);
+//        robot.BRight.setPower(0);
+//        runtime.reset();
  /*      // 4. Move forward X seconds
         robot.FLeft.setPower(FORWARD_SPEED);
         robot.FRight.setPower(FORWARD_SPEED);
@@ -83,7 +99,6 @@ public class JasonAutonomous extends LinearOpMode {
         // 5. Lower Grabber, Put down block
 
         // 6. and on and on.
-
 
   */
     }
